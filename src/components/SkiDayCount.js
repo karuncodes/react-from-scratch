0dbd4c64adc6600c6a0c9db1705504818a41406e
Karun <karun.kamal@ril.com>
2018-08-03 18:19:48 +0530

    feat: Added React-icons to the project

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,4 +1,8 @@
 import { Component } from 'react'
+import  { FaCalendar as Cal }  from 'react-icons/fa'
+import  { MdTerrain as Pahaad }  from 'react-icons/md'
+import  { TiWeatherSnow as SnowFlake }  from 'react-icons/ti'
+
 
 const percentToDecimal = decimal => {
     return (( decimal * 100)+'%')
@@ -10,23 +14,26 @@ const calcGoalProgress = (total, goal) => {
 
 
 export const SkiDayCount = ({ total, powder, backcountry, goal}) => (
-        <div className="ski-day-count">
-            <div className="total-days">
-                <span>{total}</span>
-                <span>days</span>
-            </div>
-            <div className="powder-days">
-                <span>{powder}</span>
-                <span>days</span>
-            </div>
-            <div className="backcountry-days">
-                <span>{backcountry}</span>
-                <span>days</span>
-            </div>
-            <div>
-                <span>
-                    {calcGoalProgress(total, goal)}
-                </span>
-            </div>
+    <div className="ski-day-count">
+        <div className="total-days">
+            <span>{total}</span>
+            <Cal />
+            <span>days</span>
         </div>
-    )
+        <div className="powder-days">
+            <span>{powder}</span>
+            <SnowFlake />
+            <span>days</span>
+        </div>
+        <div className="backcountry-days">
+            <span>{backcountry}</span>
+            <Pahaad />
+            <span>days</span>
+        </div>
+        <div>
+            <span>
+                {calcGoalProgress(total, goal)}
+            </span>
+        </div>
+    </div>
+)
